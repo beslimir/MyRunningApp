c35cf38146453e4d9d0b5e81d6c8c2fef2df8957
beslimir <marin.beslo@gmail.com>
2022-11-14 13:28:45 +0100

    Add Database with DAO and Entity

---
+++ app/src/main/java/com/beslimir/myrunningapp/db/Converters.kt
@@ -0,0 +1,25 @@
+package com.beslimir.myrunningapp.db
+
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import androidx.room.TypeConverter
+import androidx.room.TypeConverters
+import java.io.ByteArrayOutputStream
+
+class Converters {
+
+    @TypeConverter
+    fun fromBitmap(bmp: Bitmap): ByteArray {
+        val outputStream = ByteArrayOutputStream()
+        bmp.compress(Bitmap.CompressFormat.PNG, 100, outputStream)
+
+        return outputStream.toByteArray()
+    }
+
+    @TypeConverter
+    fun toBitmap(byteArray: ByteArray): Bitmap {
+
+        return BitmapFactory.decodeByteArray(byteArray, 0, byteArray.size)
+    }
+
+}

bc369018a352d98e64a78f185113efcb0ed0d6ec
beslimir <marin.beslo@gmail.com>
2022-12-15 13:09:21 +0100

    Save a run in the DataBase

--- app/src/main/java/com/beslimir/myrunningapp/presentation/main_screen/MainViewModel.kt
+++ app/src/main/java/com/beslimir/myrunningapp/presentation/main_screen/MainViewModel.kt
@@ -1,8 +1,11 @@
 package com.beslimir.myrunningapp.presentation.main_screen
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.beslimir.myrunningapp.data.db.Run
 import com.beslimir.myrunningapp.data.repository.MainRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -10,6 +13,8 @@ class MainViewModel @Inject constructor(
     private val mainRepository: MainRepository
 ): ViewModel() {
 
-
+    fun insertRun(run: Run) = viewModelScope.launch {
+        mainRepository.insertRun(run)
+    }
 
 }
